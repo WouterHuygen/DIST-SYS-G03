0524a5c5d152be72633270ebd78d05a056e77b07
Jorren schepers <jorren.schepers@ichoosr.com>
2020-04-27 11:16:13 +0200

    added nodeobject and beans for listening and joining

--- Project/Node/src/main/java/group1/dist/node/NodeApplication.java
+++ Project/Node/src/main/java/group1/dist/node/NodeApplication.java
@@ -1,13 +1,44 @@
 package group1.dist.node;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NodeApplication {
+
+    @Autowired
+    private ApplicationArguments args;
 
     public static void main(String[] args) {
         SpringApplication.run(NodeApplication.class, args);
     }
 
+    @Bean
+    public NodeInfo nodeInfo(){
+        if (args.containsOption("name")){
+            String name = args.getOptionValues("name").get(0);
+            System.out.println("Creating nodeInfo object with name: " + name);
+            return new NodeInfo(name);
+        }
+        String standardName = "StandardNodeName"; //TODO: standard node name
+        System.out.println("Creating nodeInfo object with name: " + standardName);
+        return new NodeInfo(standardName);
+    }
+
+    @Bean
+    public DiscoveryService discoveryService(){
+        return new DiscoveryService();
+    }
+
+    @Bean
+    public void startDiscovery(){
+        if (discoveryService().sendJoin()) {
+            System.out.println("join sent successfully");
+        }
+        System.out.println("started listening");
+        discoveryService().listen();
+    }
 }
