686d43063a196ff5bb6024c90774a22660c8b228
Jorren schepers <jorren.schepers@ichoosr.com>
2020-05-04 09:19:12 +0200

    moved handleJoinThread in naming server to method in UDPListener

--- Project/Naming-Server/naming/src/main/java/group1/dist/naming/UDPListener.java
+++ Project/Naming-Server/naming/src/main/java/group1/dist/naming/UDPListener.java
@@ -4,8 +4,10 @@ import org.springframework.context.ApplicationContext;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.nio.charset.StandardCharsets;
 
 import static group1.dist.naming.DiscoveryService.*;
 
@@ -35,12 +37,42 @@ public class UDPListener implements Runnable{
                 System.out.println("\nReceived packet from: " + packet.getAddress());
                 System.out.println("\nMessage: \"" + data + "\"\n");
                 if (data.contains("Joining")){
-                    HandleJoinThread handleJoinThread = new HandleJoinThread(data.substring(data.indexOf(':')+2, data.indexOf(',')), packet.getAddress(), context); // TODO: json
-                    handleJoinThread.start();
+                    handleJoin(data.substring(data.indexOf(':')+2, data.indexOf(',')), packet.getAddress());
+                    //HandleJoinThread handleJoinThread = new HandleJoinThread(data.substring(data.indexOf(':')+2, data.indexOf(',')), packet.getAddress(), context); // TODO: json
+                    //handleJoinThread.start();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void handleJoin(String nodeName, InetAddress ipAddress) {
+        MapManager mapManager = context.getBean(MapManager.class);
+        int existingNodes = mapManager.getMap().size();
+        String response = "Response from: Naming Server\nExisting nodes: " + existingNodes;
+        if (mapManager.addNode(new Node(nodeName, ipAddress.getHostAddress()))) {
+            System.out.println("Added node: " + nodeName); //TODO: node.toString()
+        }
+        else {
+            if (!mapManager.getMap().get(Integer.toString(Node.calculateHash(nodeName))).getIp().equals(ipAddress.getHostAddress())) {
+                int i = 0;
+                do {
+                    i++;
+                    nodeName = nodeName + i;
+                } while (mapManager.addNode(new Node(nodeName, ipAddress.getHostAddress())));
+                response += "\nNew name: " + nodeName;
+            }
+        }
+        System.out.println("sending response: \"" + response + "\"");
+        try (DatagramSocket unicastSocket = new DatagramSocket(ACK_PORT)){
+            byte[] data = response.getBytes(StandardCharsets.UTF_8);
+            DatagramPacket packet = new DatagramPacket(data, data.length, ipAddress, ACK_PORT);
+            unicastSocket.send(packet);
+            System.out.println("response sent");
+        } catch (IOException e){
+            System.out.println("failed to send ack");
+            e.printStackTrace();
         }
     }
 }
