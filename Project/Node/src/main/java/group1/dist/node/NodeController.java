6d11ca2005418aae4ac3e335bbf2208a5803e41d
Jorren schepers <jorren.schepers@ichoosr.com>
2020-05-23 17:55:57 +0200

    added nodeController with getNodeInfo

--- Project/Node/src/main/java/group1/dist/node/NodeController.java
+++ Project/Node/src/main/java/group1/dist/node/NodeController.java
@@ -1,4 +1,22 @@
 package group1.dist.node;
 
+import group1.dist.model.NodeInfo;
+import group1.dist.model.StatusObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
 public class NodeController {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @GetMapping("/info")
+    public StatusObject<NodeInfo> getNodeInfo() {
+        return new StatusObject<>(true, "info found", context.getBean(NodeInfo.class));
+    }
 }
